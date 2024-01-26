16c3cfef87ce0f9d6fce3e50089f023f541c7306
barufe <belkisarufe@gmail.com>
2024-01-25 23:37:28 -0500

    se implementan las configuraciones básicas de la app

---
+++ YoutubeClone/YoutubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeClone
+//
+//  Created by Germain Seijas on 25/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
